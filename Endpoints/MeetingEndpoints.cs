c6c8096057f5a08f11f4a48133c45c29cba3e00b
Hoang-Lam104 <hoanglam01042001@gmail.com>
2024-08-08 16:13:33 +0700

    new feature

--- Endpoints/MeetingEndpoints.cs
+++ Endpoints/MeetingEndpoints.cs
@@ -2,6 +2,10 @@
 using QLGB.API.Data;
 using QLGB.API.Dtos;
 using QLGB.API.Models;
+using NPOI.XSSF.UserModel;
+using NPOI.SS.UserModel;
+using System;
+using System.IO;
 
 namespace QLGB.API.Endpoints;
 
@@ -168,5 +172,96 @@ public static class MeetingEndpoints
 
             return Results.Ok(attendeePaging);
         }).RequireAuthorization();
+
+        endpoints.MapGet("api/meetings/{id}/export", async (int id, AppDbContext dbContext) =>
+        {
+            var meeting = await dbContext.Meetings.FindAsync(id);
+
+            if (meeting == null) return Results.NotFound();
+
+            var attendees = await dbContext.Attendees
+                .Where(a => a.MeetingId == id)
+                .ToListAsync();
+
+            var users = new List<AttendeeDto>();
+            var departments = dbContext.Departments;
+            var reasons = dbContext.Reasons;
+            var rooms = dbContext.Rooms;
+
+            foreach (var item in attendees)
+            {
+                var userEntity = dbContext.Users.Find(item.UserId);
+                var userDto = new AttendeeDto
+                (
+                    Id: userEntity!.Id,
+                    Fullname: userEntity!.Fullname,
+                    DepartmentId: userEntity.DepartmentId,
+                    Position: userEntity.Position,
+                    RoomId: item.RoomId,
+                    Status: item.Status!,
+                    ReasonId: item.ReasonId,
+                    AnotherReason: item.AnotherReason
+                );
+
+                {
+                    users.Add(userDto);
+                }
+            }
+
+            // Tạo workbook mới
+            IWorkbook workbook = new XSSFWorkbook();
+
+            // Tạo sheet mới
+            ISheet sheet = workbook.CreateSheet("Họp giao ban");
+
+            // Tạo hàng đầu tiên và gán giá trị vào các ô
+            IRow row1 = sheet.CreateRow(0);
+            row1.CreateCell(0).SetCellValue("STT");
+            row1.CreateCell(1).SetCellValue("Họ tên");
+            row1.CreateCell(2).SetCellValue("Chức vụ");
+            row1.CreateCell(3).SetCellValue("Phòng ban");
+            row1.CreateCell(4).SetCellValue("Tình trạng");
+            row1.CreateCell(5).SetCellValue("Hội trường");
+            row1.CreateCell(6).SetCellValue("Lý do vắng");
+
+            for (var i = 0; i < users.Count(); i++)
+            {
+                IRow row = sheet.CreateRow(i + 1);
+                row.CreateCell(0).SetCellValue(i + 1);
+                row.CreateCell(1).SetCellValue(users[i].Fullname);
+                row.CreateCell(2).SetCellValue(users[i].Position);
+                row.CreateCell(3).SetCellValue(departments.Find(users[i].DepartmentId)?.Name);
+                row.CreateCell(4).SetCellValue(users[i].Status);
+                row.CreateCell(5).SetCellValue(rooms.Find(users[i].RoomId)?.Name);
+                row.CreateCell(6).SetCellValue(reasons.Find(users[i].ReasonId)?.Name);
+            };
+
+            // var memoryStream = new MemoryStream();
+            // workbook.Write(memoryStream);
+            // memoryStream.Position = 0;
+
+            // // Đóng workbook
+            // workbook.Close();
+
+            // return Results.File(memoryStream, "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", "example.xlsx");
+
+            // Đường dẫn tới file xuất
+            string downloadsPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), "Downloads");
+            string filePath = Path.Combine(downloadsPath, "example.xlsx");
+
+            // Ghi workbook vào file
+            using (FileStream stream = new FileStream(filePath, FileMode.Create, FileAccess.Write))
+            {
+                workbook.Write(stream);
+            }
+
+            // Đóng workbook
+            workbook.Close();
+
+            return Results.Ok(new
+            {
+                message = "Tải xuống thành công"
+            });
+        });
     }
 }
